d47eeae9e12530600e7982c651929dbabc19d93d
Bruce <molaalishefket@gmail.com>
2024-03-18 22:04:10 +0200

    d

--- client/src/component/home/barcode/barcode.jsx
+++ client/src/component/home/barcode/barcode.jsx
@@ -4,18 +4,18 @@ import style from "./barcode.scss";
 export const Barcode = () => {
   return (
     <>
-      <div className={style["barcode"]}>
-        <div className={`${style["barcode__image-1"]} ${style["barcode_img"]}`}>
+      <div className={["barcode"]}>
+        <div className={`${["barcode__image-1"]} ${["barcode_img"]}`}>
           <img src="/src/assets/images/Картина1.png" alt="Duet Bisera and Lidia" />
         </div>
-        <div className={style["barcode__image-2"]}>
+        <div className={["barcode__image-2"]}>
           {/* Add your second image here if needed */}
         </div>
-        <div className={style["barcode__text"]}>
+        <div className={["barcode__text"]}>
           <h2>Дует Бисера и Лидия</h2>
           <p>+359 88 994 3359 | +359 88 849 5500</p>
           <p>bisera.lidia@gmail.com</p>
-          <button className={style["barcode-btn"]}>Позвънете</button>
+          <button className={["barcode-btn"]}>Позвънете</button>
         </div>
       </div>
     </>
